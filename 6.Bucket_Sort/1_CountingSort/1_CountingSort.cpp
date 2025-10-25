7a5016330066a235c2c2bf0a67ae560d84e054a7
AsanHO <gorma00@naver.com>
2025-10-25 17:42:45 +0900

    cmt : counting sort && radix sort

--- 6.Bucket_Sort/1_CountingSort/1_CountingSort.cpp
+++ 6.Bucket_Sort/1_CountingSort/1_CountingSort.cpp
@@ -1,64 +1,66 @@
-﻿#include <iostream>
-#include <vector>
-#include <random>
-#include <limits>
+﻿#include <chrono>
 #include <fstream>
-#include <chrono>
-#include <numeric>
 #include <iomanip>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <random>
+#include <vector>
 using namespace std;
 
-void Print(vector<int>& arr)
-{
-	for (auto a : arr)
-		if (a == -1)
-			cout << "X ";
-		else
-			cout << a << " ";
-	cout << endl;
+void Print(vector<int>& arr) {
+    for (auto a : arr)
+        if (a == -1)
+            cout << "X ";
+        else
+            cout << a << " ";
+    cout << endl;
 }
 
 // 입력은 0 이상 k 이하의 정수, CSLR 8.2
 // 결과를 반환하는 구조 (In-place가 아님)
-vector<int> CountingSort(const vector<int>& arr, int k)
-{
-	vector<int> count(k + 1, 0); // 0이상 k이하니까 k + 1개를 0으로 초기화
+vector<int> CountingSort(const vector<int>& arr, int k) {
+    vector<int> count(k + 1, 0);  // 0이상 k이하니까 k + 1개를 0으로 초기화
 
-	// TODO:
+    // TODO:
+    for (int num : arr) {
+        count[num] += 1;
+    }
+    for (int i = 1; i < k + 1; i++) {
+        count[i] += count[i - 1];
+    }
+    cout << "Count: ";
+    Print(count);
 
-	cout << "Count: ";
-	Print(count);
+    vector<int> output(arr.size(), -1);  // -1로 초기화하는 것은 디버깅용
 
-	vector<int> output(arr.size(), -1); // -1로 초기화하는 것은 디버깅용
+    // 역순으로 복사
+    for (int i = output.size() - 1; i >= 0; i--) {
+        // TODO:
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+        cout << "Count: ";
+        Print(count);
 
-	// 역순으로 복사
-	for (int i = output.size() - 1; i >= 0; i--)
-	{
-		// TODO:
+        cout << "Output: ";
+        Print(output);
+    }
 
-		cout << "Count: ";
-		Print(count);
-
-		cout << "Output: ";
-		Print(output);
-	}
-
-	return output;
+    return output;
 }
 
-int main()
-{
-	vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
+int main() {
+    vector<int> arr = {2, 5, 3, 0, 2, 3, 0, 3};
 
-	Print(arr);
+    Print(arr);
 
-	int k = *std::max_element(arr.begin(), arr.end());
-	// 아래 CountingSort()에서 사용할 가장 작은 인덱스(arr에서 가장 작은 값)은 0
-	// 가장 큰 인덱스(arr에서 가장 큰 값)이 k
+    int k = *std::max_element(arr.begin(), arr.end());
+    // 아래 CountingSort()에서 사용할 가장 작은 인덱스(arr에서 가장 작은 값)은 0
+    // 가장 큰 인덱스(arr에서 가장 큰 값)이 k
 
-	vector<int> result = CountingSort(arr, k);
+    vector<int> result = CountingSort(arr, k);
 
-	Print(result);
+    Print(result);
 
-	return 0;
+    return 0;
 }
--- 6.Bucket_Sort/2_RadixSort/2_RadixSort.cpp
+++ 6.Bucket_Sort/2_RadixSort/2_RadixSort.cpp
@@ -1,55 +1,59 @@
-﻿#include <iostream>
-#include <vector>
-#include <random>
-#include <limits>
+﻿#include <chrono>
 #include <fstream>
-#include <chrono>
-#include <numeric>
 #include <iomanip>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <random>
+#include <vector>
 using namespace std;
 
-void Print(vector<int>& arr)
-{
-	for (auto& a : arr)
-		cout << a << " ";
-	cout << endl;
+void Print(vector<int>& arr) {
+    for (auto& a : arr) cout << a << " ";
+    cout << endl;
 }
 
 // 편의상 결과가 arr에 저장되도록 바꿨습니다.
-void CountingSort(vector<int>& arr, int k, int exp)
-{
-	vector<int> temp = arr; // 복사
+void CountingSort(vector<int>& arr, int k, int exp) {
+    vector<int> temp = arr;  // 복사
 
-	vector<int> count(k + 1, 0);
-	// TODO:
+    vector<int> count(k + 1, 0);
+    // TODO:
+    for (int num : arr) {
+        count[(num % (exp * 10)) / exp] += 1;
+    }
+    for (int i = 1; i < k + 1; i++) {
+        count[i] += count[i - 1];
+    }
+    Print(count);
+    for (int i = arr.size() - 1; i >= 0; i--) {
+        // TODO:
+        int expTarget = (temp[i] % (exp * 10)) / exp;
+        cout << "arr[" << count[expTarget] << "] = " << temp[i] << endl;
 
-	for (int i = arr.size() - 1; i >= 0; i--)
-	{
-		// TODO:
-	}
+        arr[count[expTarget] - 1] = temp[i];
+        count[expTarget]--;
+    }
 }
 
-void RadixSort(vector<int>& arr)
-{
-	int k = 9; // 0 이상 9 이하
-	int m = *max_element(arr.begin(), arr.end());
+void RadixSort(vector<int>& arr) {
+    int k = 9;  // 0 이상 9 이하
+    int m = *max_element(arr.begin(), arr.end());
 
-	//for (TODO)
-	//{
-	//	cout << "exp = " << exp << endl;
-	//	TODO:
-	//	Print(arr);
-	//}
+    for (int exp = 1; m / exp > 0; exp *= 10) {
+        cout << "exp = " << exp << endl;
+        CountingSort(arr, k, exp);
+        Print(arr);
+    }
 }
 
-int main()
-{
-	vector<int> arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
-	//vector<int> arr = { 2, 5, 3, 0, 2, 3, 0, 3 };
+int main() {
+    vector<int> arr = {170, 45, 75, 90, 802, 24, 2, 66};
+    // vector<int> arr = {2, 5, 3, 0, 2, 3, 0, 3};
 
-	Print(arr);
+    Print(arr);
 
-	RadixSort(arr);
+    RadixSort(arr);
 
-	return 0;
+    return 0;
 }
