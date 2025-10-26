39640dce8b904c601ab9150a44c7c5522fd4ecb2
AsanHO <gorma00@naver.com>
2025-10-26 16:33:54 +0900

    cmt : BucketSort

--- 6.Bucket_Sort/4_BucketSort/4_BucketSort.cpp
+++ 6.Bucket_Sort/4_BucketSort/4_BucketSort.cpp
@@ -1,71 +1,77 @@
-﻿#include <iostream>
-#include <vector>
-#include <random>
-#include <limits>
+﻿#include <chrono>
 #include <fstream>
-#include <chrono>
-#include <numeric>
 #include <iomanip>
+#include <iostream>
+#include <limits>
+#include <numeric>
+#include <random>
+#include <vector>
 using namespace std;
 
-void Print(vector<float>& arr)
-{
-	for (auto& a : arr)
-		cout << a << " ";
-	cout << endl;
+void Print(vector<float>& arr) {
+    for (auto& a : arr) cout << a << " ";
+    cout << endl;
 }
 
-void PrintBuckets(vector<vector<float>>& buckets)
-{
-	for (int i = 0; i < buckets.size(); i++)
-	{
-		cout << i << ": ";
-		for (int j = 0; j < buckets[i].size(); j++)
-			cout << buckets[i][j] << " ";
-		cout << endl;
-	}
+void PrintBuckets(vector<vector<float>>& buckets) {
+    for (int i = 0; i < buckets.size(); i++) {
+        cout << i << ": ";
+        for (int j = 0; j < buckets[i].size(); j++) cout << buckets[i][j] << " ";
+        cout << endl;
+    }
 }
 
 // 다른 정렬을 사용해도 됩니다.
-void InsertionSort(vector<float>& bucket)
-{
-	for (int i = 1; i < bucket.size(); ++i) {
-		float key = bucket[i];
-		int j = i - 1;
-		while (j >= 0 && bucket[j] > key) {
-			bucket[j + 1] = bucket[j];
-			j--;
-		}
-		bucket[j + 1] = key;
-	}
+void InsertionSort(vector<float>& bucket) {
+    for (int i = 1; i < bucket.size(); ++i) {
+        float key = bucket[i];
+        int j = i - 1;
+        while (j >= 0 && bucket[j] > key) {
+            bucket[j + 1] = bucket[j];
+            j--;
+        }
+        bucket[j + 1] = key;
+    }
 }
 
-void BucketSort(vector<float>& arr, int num_buckets)
-{
-	vector<vector<float>> buckets(num_buckets);
+void BucketSort(vector<float>& arr, int num_buckets) {
+    vector<vector<float>> buckets(num_buckets);
 
-	// TODO:
+    // TODO:
+    for (float num : arr) {
+        int hash = (int)(num * 10);
+        buckets[hash].push_back(num);
+    }
 
-	cout << "Before sorting" << endl;
-	PrintBuckets(buckets);
+    cout << "Before sorting" << endl;
+    PrintBuckets(buckets);
 
-	// TODO:
+    // TODO:
+    for (vector<float>& bucket : buckets) {
+        InsertionSort(bucket);
+    }
 
-	cout << "After sorting" << endl;
-	PrintBuckets(buckets);
+    cout << "After sorting" << endl;
+    PrintBuckets(buckets);
 
-	// TODO:
+    // TODO:
+    int i = 0;
+    for (vector<float>& bucket : buckets) {
+        for (float& num : bucket) {
+            arr[i] = num;
+            i++;
+        }
+    }
 }
 
-int main()
-{
-	vector<float> arr = { 0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.67f };
+int main() {
+    vector<float> arr = {0.78f, 0.17f, 0.39f, 0.26f, 0.72f, 0.94f, 0.21f, 0.12f, 0.23f, 0.67f};
 
-	Print(arr);
+    Print(arr);
 
-	BucketSort(arr, 10);
+    BucketSort(arr, 10);
 
-	Print(arr);
+    Print(arr);
 
-	return 0;
+    return 0;
 }
